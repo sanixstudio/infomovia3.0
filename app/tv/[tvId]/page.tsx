2eba2650841eaf3f681380d68bfd77fddea24429
Adnan Niaz <sanixstudio@gmail.com>
2023-06-03 17:25:38 -0700

    fix: removed issue for deployment

--- app/tv/[tvId]/page.tsx
+++ app/tv/[tvId]/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
   return (
     <div className="min-h-[1000px] navbar max-w -[1440px] mx-auto my-0">
       <div className="w-full max-w-[1200px] mx-auto">
-        <FullPosterCard
+        {/* <FullPosterCard
           movie={{
             title: "",
             release_date: 0,
@@ -24,7 +24,7 @@ const page = () => {
             overview: "",
             genres: [],
           }}
-        />
+        /> */}
       </div>
     </div>
   );
