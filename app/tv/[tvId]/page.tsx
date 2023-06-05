1463e4918941ebc08f0692b16b6585b1520a810c
Adnan Niaz <sanixstudio@gmail.com>
2023-06-05 11:17:25 -0700

    fix: fixed npm issue

--- app/tv/[tvId]/page.tsx
+++ app/tv/[tvId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FullCelebCard, FullPosterCard } from "@/components";
+import { FullPosterCard } from "@/components";
 import { usePathname } from "next/navigation";
 import React from "react";
 
