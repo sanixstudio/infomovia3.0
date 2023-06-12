114a121dae43610c0f1aba0f180e844030f92e5f
Adnan Niaz <sanixstudio@gmail.com>
2023-06-12 01:47:56 -0700

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
 export const authOptions = {
   providers: [
@@ -12,11 +11,4 @@ export const authOptions = {
   secret: process.env.SECRET,
 };
 
-// const handler = NextAuth({
-//   ...authOptions,
-//   site: process.env.NEXTAUTH_URL || "http://localhost:3000",
-// });
-
 export default NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
