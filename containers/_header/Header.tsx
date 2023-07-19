b9150d57723e864454864d8ca75825f94b323f83
Adnan Niaz <sanixstudio@gmail.com>
2023-07-18 17:01:58 -0700

    merged branch

--- containers/_header/Header.tsx
+++ containers/_header/Header.tsx
@@ -1,108 +1,3 @@
-// import {
-//   Search,
-//   UserMenu,
-//   MainMenu,
-//   WishListBtn,
-//   HiddenNavbar,
-// } from "@/components";
-// import Image from "next/image";
-// import Link from "next/link";
-
-// const Logo = () => {
-//   return (
-//     <Link href="/" className="mr-8">
-//       <Image
-//         src="/images/im_logo.png"
-//         width={400}
-//         height={100}
-//         alt="infomovia  logo"
-//         className="hidden sm:block max-w-[200px] md:max-w-full"
-//       />
-//       <Image
-//         src="/images/im_icon.png"
-//         width={40}
-//         height={40}
-//         alt="infomovia logo"
-//         className="sm:hidden max-w-[200px] md:max-w-full"
-//       />
-//     </Link>
-//   );
-// };
-
-// const Header = () => {
-//   return (
-//     <div className="bg-slate-950 py-2">
-//       <div className="navbar max-w-[1440px] flex justify-between mx-auto my-0">
-//         <HiddenNavbar />
-//         <Logo />
-//         <MainMenu />
-//         <Search />
-//         <WishListBtn />
-//         <UserMenu />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-// "use client";
-// import { useState } from "react";
-// import { HiddenNavbar, Search, UserMenu, WishListBtn } from "@/components";
-// import { Navbar } from "flowbite-react";
-// import Image from "next/image";
-
-// const Logo = () => {
-//   return (
-//     <Navbar.Brand href={"/"} className="mr-8">
-//       <Image
-//         src="/images/im_logo.png"
-//         width={220}
-//         height={100}
-//         alt="infomovia logo"
-//         className="hidden sm:block max-w-[200px] md:max-w-full"
-//       />
-//       <Image
-//         src="/images/im_icon.png"
-//         width={40}
-//         height={40}
-//         alt="infomovia logo"
-//         className="sm:hidden max-w-[200px] md:max-w-full"
-//       />
-//     </Navbar.Brand>
-//   );
-// };
-
-// export default function NavbarWithDropdown() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   return (
-//     <Navbar fluid rounded className="bg-slate-800">
-//       <div className="max-w-[1440px] w-full flex justify-between mx-auto px-5">
-//         {/* <HiddenNavbar isOpen={isOpen} /> */}
-//         <Logo />
-//         <div className="hidden md:flex list-none items-center gap-5">
-//           <Navbar.Link active href="#">
-//             Home
-//           </Navbar.Link>
-//           <Navbar.Link href="#">Genre</Navbar.Link>
-//           <Navbar.Link href="#">Explore</Navbar.Link>
-//         </div>
-//         <div className="flex items-center">
-//           <Search />
-//         </div>
-//         <div className="w-[50px] flex">
-//           <WishListBtn />
-//         </div>
-//         <div className="flex md:order-2">
-//           <UserMenu />
-//           <Navbar.Toggle />
-//         </div>
-//       </div>
-//     </Navbar>
-//   );
-// }
-
 "use client";
 
 import { Search } from "@/components";
@@ -166,7 +61,10 @@ export default function NavbarWithDropdown() {
         </div>
         <Navbar.Collapse className="w-full py-5 md:py-0 pb-10">
           <div className="w-full px-5 md:flex md:items-center gap-10">
-            <Navbar.Link href="/" className="text-white hover:bg-slate-500 border-0 pl-0">
+            <Navbar.Link
+              href="/"
+              className="text-white hover:bg-slate-500 border-0 pl-0"
+            >
               <p>Home</p>
             </Navbar.Link>
             <div className="my-5 md:my-0">
